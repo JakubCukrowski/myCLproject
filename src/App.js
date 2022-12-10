9ef9b94a64610d75e62958cc698493ffadeccb1f
JakubCukrowski <j.d.cukrowski@gmail.com>
2022-12-10 14:07:19 +0100

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Routes, useParams} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Info from "./components/info/Info";
 import Gallery from "./components/gallery/Gallery";
