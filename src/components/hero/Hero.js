3627f9676975d94680807f0a36212fa2dfdac647
JakubCukrowski <j.d.cukrowski@gmail.com>
2023-02-23 22:30:19 +0100

    Hero redesigned

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Hero = () => {
-
     return (
         <>
-            <div className="hero"></div>
+            <div className="hero">
             <div className="hero-content">
                 <h1>Pracownia psychologiczna</h1>
                 <p>To miesce stworzone z miłosci do ludzi i z szacunku do ich historii.
                     Łączy nas profesjonalizm i pasja.</p>
+            </div>
             </div>
         </>
     )
