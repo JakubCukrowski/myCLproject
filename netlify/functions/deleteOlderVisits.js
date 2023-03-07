225ba0d8e570a524021960e567085191d5fdb702
JakubCukrowski <j.d.cukrowski@gmail.com>
2023-03-07 23:45:28 +0100

    For test with cron-jobs.org

--- netlify/functions/deleteOlderVisits.js
+++ netlify/functions/deleteOlderVisits.js
@@ -1,5 +1,5 @@
 import * as admin from "firebase-admin";
-const serviceAccount = require("../.netlify/psychology-77338-firebase-adminsdk-tscp5-74406d4aa3.json")
+const serviceAccount = require("../../.netlify/psychology-77338-firebase-adminsdk-tscp5-74406d4aa3.json")
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
