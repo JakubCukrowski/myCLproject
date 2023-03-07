ccdf7d4a5464721e03de092ee85e7ac8f2971c55
JakubCukrowski <j.d.cukrowski@gmail.com>
2023-03-07 23:41:38 +0100

    For test with cron-jobs.org

--- netlify/functions/deleteOlderVisits.js
+++ netlify/functions/deleteOlderVisits.js
@@ -1,6 +1,4 @@
 import * as admin from "firebase-admin";
-
-const admin = require("firebase-admin");
 const serviceAccount = require("../.netlify/psychology-77338-firebase-adminsdk-tscp5-74406d4aa3.json")
 
 admin.initializeApp({
