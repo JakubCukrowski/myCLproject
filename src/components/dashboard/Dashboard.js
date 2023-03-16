fddb4f515f397f21be611bf896b037702178cb6a
JakubCukrowski <j.d.cukrowski@gmail.com>
2023-03-16 20:08:04 +0100

    Resolved problem with hiding overflow, changed social media links

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -42,7 +42,7 @@ const Dashboard = () => {
         if (window.innerWidth <= 900) {
             setIsOpen(prev => !prev)
         }
-        if ((isOpen === false && window.innerWidth <= 900) || isBlocked === false) {
+        if ((isOpen === false && window.innerWidth <= 900)) {
             document.querySelector("body").style.overflow = "hidden"
         } else {
             document.querySelector("body").style.overflow = "auto"
@@ -87,6 +87,7 @@ const Dashboard = () => {
                 </ul>
             </div>
             <div className="top-line">
+                {isOpen ? null : <h3 className="user-display-top">{`Witaj, ${user.displayName}`}</h3>}
                 <FontAwesomeIcon className="menu-button"
                                 icon={isOpen ? faXmark : faBars}
                                 onClick={handleHamburgerMenu}
