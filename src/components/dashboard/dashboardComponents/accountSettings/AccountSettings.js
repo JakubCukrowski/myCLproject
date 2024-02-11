3548772a04fdb42c9c80926d907f0fb05bddeaf3
JakubCukrowski <j.d.cukrowski@gmail.com>
2024-02-11 16:54:03 +0100

    Added a bit of a css

--- src/components/dashboard/dashboardComponents/accountSettings/AccountSettings.js
+++ src/components/dashboard/dashboardComponents/accountSettings/AccountSettings.js
@@ -1,77 +1,93 @@
-import React, {useState} from "react";
-import {useAuth} from "../../../context/AuthContext";
-import {updateProfile, updateEmail} from "firebase/auth";
-import {doc, updateDoc} from "@firebase/firestore";
-import {db} from "../../../../firebase/firebase";
+import React, { useState } from "react";
+import { useAuth } from "../../../context/AuthContext";
+import { updateEmail } from "firebase/auth";
+import { doc, updateDoc } from "@firebase/firestore";
+import { db } from "../../../../firebase/firebase";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faUser } from "@fortawesome/free-regular-svg-icons";
 
 const AccountSettings = () => {
+  const { user } = useAuth();
+  const [editing, setEditing] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
 
-    const {user} = useAuth()
-    const [editing, setEditing] = useState(false)
-    const [userData, setUserData] = useState({
-        displayName: "",
-        email: ""
-    })
-    const userRef = doc(db, "users", user.uid)
+  const userRef = doc(db, "users", user.uid);
 
-    const handleEdit = () => {
-        setEditing(prevState => !prevState)
-    }
+  const handleEdit = () => {
+    setEditing((prevState) => !prevState);
+  };
 
-    const handleChangeData = () => {
-        updateProfile(user, {
-            displayName: userData.displayName,
-        })
-            .then(() => updateDoc(userRef, {
-                name: userData.displayName
-            }))
-            .then(() => alert("Pomyślnie zmieniono dane"))
-        updateEmail(user, userData.email)
-            .then(() => updateDoc(userRef, {
-                email: userData.email
-            }))
-        setEditing(false)
-        window.location.reload()
-    }
+  const handleChangeData = () => {
+    updateEmail(user, userEmail).then(() =>
+      updateDoc(userRef, {
+        email: userEmail,
+      })
+    );
+    setEditing(false);
+  };
 
-    const handleInputs = (e) => {
-        const { name, value } = e.target;
-        setUserData((prevState) => {
-            return {
-                ...prevState,
-                [name]: value,
-            };
-        });
-        console.log(userData.displayName, userData.email)
-    }
+  const handleInputs = (e) => {
+    setUserEmail(e.target.value);
+  };
 
-    //ADD PAST VISITS VIEW OPTION
-    //ADD PAYMENT METHOD SELECT/ OR SOMETHING
+  return (
+    <>
+      <h2>Zarządzanie kontem</h2>
+      <div className="user-data-container">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 14,
+            marginBottom: 10,
+          }}
+        >
+          <FontAwesomeIcon icon={faUser} fontSize={40} />
+          <p style={{ marginBottom: 0 }}>{user.displayName.toUpperCase()}</p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 14,
+            marginBottom: 10,
+          }}
+        >
+          <FontAwesomeIcon icon={faEnvelope} fontSize={40} />
+          <p style={{ marginBottom: 0 }}>Email: {user.email}</p>
+        </div>
+        {editing ? (
+          <input
+            onChange={handleInputs}
+            value={userEmail}
+            type="email"
+            name="email"
+          />
+        ) : null}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 14,
+            marginBottom: 10,
+          }}
+        >
+          <button onClick={handleEdit} className="edit-btn">
+            {editing ? "Anuluj" : "Zmień email"}
+          </button>
+          {editing ? (
+            <button onClick={handleChangeData} className="edit-btn">
+              Zapisz
+            </button>
+          ) : null}
+        </div>
+        <button className="delete-btn">Usuń konto</button>
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            <h2>Zarządzanie kontem</h2>
-            <div className="user-data-container">
-                <div>
-                    <FontAwesomeIcon icon={faUser} fontSize={40}/>
-                    <p>Imię: {user.displayName.toUpperCase()}</p>
-                </div>
-                {editing ? <input onChange={handleInputs} value={userData.displayName} type="text" name="displayName"/> : null}
-                <div>
-                    <FontAwesomeIcon icon={faEnvelope} fontSize={40}/>
-                    <p>Email: {user.email}</p>
-                </div>
-                {editing ? <input onChange={handleInputs} value={userData.email} type="email" name="email"/> : null}
-                <div>
-                    <button onClick={handleEdit} className="edit-btn">{editing ? "Anuluj" : "Edytuj"}</button>
-                    {editing ? <button onClick={handleChangeData} className="edit-btn">Zapisz</button> : null}
-                </div>
-                <button className="delete-btn">Usuń konto</button>
-            </div>
-        </>
-    )
-}
-
-export default AccountSettings
+export default AccountSettings;
